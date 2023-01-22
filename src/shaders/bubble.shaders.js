c45bdc321b68b62c1f0f5147c5e87f7a24fba193
Gabriel Zhong <gabriel@Gabriels-MacBook-Pro.local>
2023-01-23 04:44:18 +0900

    fix code format

--- src/shaders/bubble.shaders.js
+++ src/shaders/bubble.shaders.js
@@ -21,32 +21,32 @@ const vec3 wavelengths = vec3(620.0, 530.0, 485.0); // nm
 vec3 getInterferenceColor() {
   vec3 interferenceColor = u_directionalLightColor;
 
-  float cos_theta = max( dot(normalize(u_directionalLightPos), v_mvNormal), dot(normalize(u_directionalLightPos), -v_mvNormal) );
-  float sin_phi_squared = ( 1.0 - pow( cos_theta, 2.0 ) ) / pow( u_refractiveIndex, 2.0 );
-  float distanceDiff = ( 2.0 * u_surfaceThickness * sqrt(sin_phi_squared) ) / sqrt( 1.0 - sin_phi_squared );
+  float cos_theta = max(dot(normalize(u_directionalLightPos), v_mvNormal), dot(normalize(u_directionalLightPos), -v_mvNormal) );
+  float sin_phi_squared = (1.0 - pow(cos_theta, 2.0)) / pow(u_refractiveIndex, 2.0);
+  float distanceDiff = (2.0 * u_surfaceThickness * sqrt(sin_phi_squared)) / sqrt(1.0 - sin_phi_squared);
 
-  interferenceColor *= 0.5 * sqrt( 2.0 + 2.0 * cos( 2.0 * PI * distanceDiff / wavelengths ) );
+  interferenceColor *= 0.5 * sqrt(2.0 + 2.0 * cos(2.0 * PI * distanceDiff / wavelengths));
 
   return interferenceColor;
 }
 
 void main() {
   vec3 ambientColor = u_ambientLightColor * u_materialColor;
-  vec3 diffuseColor = max( 0.0, dot( normalize(u_directionalLightPos), v_mvNormal ) ) * u_directionalLightColor * u_materialColor;
+  vec3 diffuseColor = max(0.0, dot(normalize(u_directionalLightPos), v_mvNormal)) * u_directionalLightColor * u_materialColor;
   vec3 interferenceColor = getInterferenceColor();
-  vec3 lightColor = mix( u_backgroundColor, ambientColor + diffuseColor + interferenceColor, 0.1 );
+  vec3 lightColor = mix(u_backgroundColor, ambientColor + diffuseColor + interferenceColor, 0.1);
   vec2 uv = v_Uv;
   // left side of bubble
-  if( uv.x < 0.25 )
-    uv = vec2( 0.25 - uv.x, ( 0.6 - uv.y ) * 2.0 ); // flip side up and move to the bottom left.
+  if(uv.x < 0.25)
+    uv = vec2(0.25 - uv.x, (0.6 - uv.y) * 2.0); // flip side up and move to the bottom left.
   // right side of bubble
   else
-    uv = vec2( uv.x, ( uv.y - 0.4 ) * 2.0 ); // move to the top right.
-  uv = vec2( fract( uv.x * 4.0 ), uv.y );
+    uv = vec2(uv.x, ( uv.y - 0.4 ) * 2.0); // move to the top right.
+  uv = vec2(fract( uv.x * 4.0 ), uv.y);
   // distortion
-  vec2 uvOffset = 0.01 * vec2( cos( uv.y * 20. ), sin( uv.x * 20. ) );
-  vec3 color = texture2D( u_texture, uv + uvOffset ).rgb;
-  gl_FragColor.rgb = mix( lightColor, color, 0.3 );
+  vec2 uvOffset = 0.01 * vec2(cos(uv.y * 20.), sin(uv.x * 20.));
+  vec3 color = texture2D(u_texture, uv + uvOffset).rgb;
+  gl_FragColor.rgb = mix(lightColor, color, 0.3);
   gl_FragColor.a = u_alpha;
 }`,
   vert: `
